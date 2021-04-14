27dd6d5fba64da35e627d2d24a084e115eebd302
yuncheng <yun13cheng@gmail.com>
2021-04-14 15:23:43 -0400

    intial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Navigation from './Components/Navigation/Navigation';
+import Signin from './Components/Signin/Signin';
+import Register from './Components/Register/Register';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 
-function App() {
+const app=new Clarifai.App({
+  apiKey:'db3eee763e754f4c995dc14f1b63da61'
+});
+
+
+const particlesOptions={
+  particles: {
+    number: {
+      value:50,
+      density: {
+        enable:true,
+        value_area:500
+      }
+    }
+}
+}
+
+class App extends Component {
+
+  constructor (){
+    super();
+    this.state={
+      input: '',
+      imageUrl:'',
+      box: {},
+      route: 'signin'
+    }
+  }
+
+ 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+
+
+  onButtonSubmit=() =>{
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+     this.state.input)
+     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+     .catch(err => console.log(err));
+    }
+  
+    onRouteChange=(route)=>{
+      this.setState({route: route});
+    }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+                params={particlesOptions} />
+      <Navigation onRouteChange={this.onRouteChange}/>
+      {this.state.route==='signin' 
+      ? <Signin onRouteChange={this.onRouteChange}/>
+      :<div>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/> 
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
+      </div>
+      }
     </div>
   );
+    }
 }
 
 export default App;
